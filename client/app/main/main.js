9b2cd43efd0ca52104e2ff8ec7a0609c170ebd89
Raghu V N <raghuvenmarathoor@gmail.com>
2015-05-01 20:59:39 +0530

    First commit

---
+++ client/app/main/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('ideabuilderApp')
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'app/main/main.html',
+        controller: 'MainCtrl'
+      });
+  });
